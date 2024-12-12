c6e53c8029087a945a7bddb65c8d98e88e9ac378
vaibhavpatil2734 <vaibhavpatil.2734@gmail.com>
2024-12-12 17:49:58 +0530

    origin changed

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // CORS Middleware - Set allowed origin
 app.use(
   cors({                          
-    origin: ['http://localhost:5173', 'https://wellnessnest1.netlify.app'], // Use the correct frontend URL directly
+    origin: ['http://localhost:5173', 'https://wellnessnest.netlify.app'], // Use the correct frontend URL directly
     methods: ["GET", "POST", "PUT", "DELETE"], // Allow common HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
   })
